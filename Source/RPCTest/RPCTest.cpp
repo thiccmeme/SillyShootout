dc8722f08c80ec37a43a6ec460eee7c757035a98
Ayden <Theownage79@gmail.com>
2025-02-17 22:00:27 -0700

    Get test projectile with physics working on client and server

---
+++ Source/RPCTest/RPCTest.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RPCTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RPCTest, "RPCTest" );
+ 
